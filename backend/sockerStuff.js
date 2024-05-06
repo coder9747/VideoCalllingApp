ad98c26e9067741f07e5a3c8ba10ffb51bb7e3c3
Your Name <pratyushkarn007@gmail.com>
2024-05-06 23:19:25 +0530

    changed application logics for offer and answer

--- backend/sockerStuff.js
+++ backend/sockerStuff.js
@@ -13,5 +13,28 @@ io.on("connection", (socket) => {
             socket.to(data).emit("peerJoin")
             socket.join(data);
         }
+    });
+    socket.on("peerJoined", (room) => {
+        socket.to(room).emit("peerJoined");
+    });
+    socket.on("offerFromPeer", (data) => {
+
+        const { offer, room } = data;
+        console.log(offer,room);
+        offers[room] = {
+            offer: offer,
+            answer: null,
+            answerIce: [],
+            offerIce: [],
+        }
+        socket.to(room).emit("offerFromPeer", offer);
+    });
+    socket.on("answerFromUser", ({ answer, room }) => {
+        offers[room].answer = answer;
+        socket.to(room).emit("answerFromUser", answer);
     })
+    socket.on("iceCandidate", ({ candidate, roomId }) => {
+        socket.to(roomId).emit("iceCandidate",candidate);
+    })
+
 })
