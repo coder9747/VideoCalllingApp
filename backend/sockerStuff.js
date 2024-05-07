5067289470c2b2789823eaa6ceb4485c0aa17a38
Your Name <pratyushkarn007@gmail.com>
2024-05-07 14:31:52 +0530

    changed all front logic

--- backend/sockerStuff.js
+++ backend/sockerStuff.js
@@ -4,37 +4,23 @@ let offers = []
 
 
 io.on("connection", (socket) => {
-    socket.on("joinRoom", async (data) => {
-        const numberOfPeronInRoom = (await io.in(data).fetchSockets()).length;
-        if (numberOfPeronInRoom == 0) {
-            socket.join(data);
-        }
-        else if (numberOfPeronInRoom == 1) {
-            socket.to(data).emit("peerJoin")
-            socket.join(data);
-        }
-    });
-    socket.on("peerJoined", (room) => {
-        socket.to(room).emit("peerJoined");
-    });
-    socket.on("offerFromPeer", (data) => {
-
-        const { offer, room } = data;
-        console.log(offer,room);
-        offers[room] = {
-            offer: offer,
-            answer: null,
-            answerIce: [],
-            offerIce: [],
-        }
-        socket.to(room).emit("offerFromPeer", offer);
-    });
-    socket.on("answerFromUser", ({ answer, room }) => {
-        offers[room].answer = answer;
-        socket.to(room).emit("answerFromUser", answer);
+    socket.on("joinRoom", (room) => {
+        socket.join(room);
     })
-    socket.on("iceCandidate", ({ candidate, roomId }) => {
+    socket.on('ready', (room) => {
+        socket.to(room).emit("ready");
+    })
+    socket.on("incommingOffer",({offer,room})=>
+    {
+        socket.to(room).emit("incommingOffer",offer)
+    })
+    socket.on("incommingAnswer",({answer,room})=>
+    {
+        console.log(room);
+        socket.to(room).emit("incommingAnswer",answer);
+    })
+    socket.on("iceCandidate",({candidate,roomId})=>
+    {
         socket.to(roomId).emit("iceCandidate",candidate);
     })
-
 })
