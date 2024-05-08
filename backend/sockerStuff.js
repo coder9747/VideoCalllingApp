39fa420305297cf14d73084465a382599970940c
Your Name <pratyushkarn007@gmail.com>
2024-05-08 17:37:29 +0530

    builded full application video calling screen sharing

--- backend/sockerStuff.js
+++ backend/sockerStuff.js
@@ -10,17 +10,17 @@ io.on("connection", (socket) => {
     socket.on('ready', (room) => {
         socket.to(room).emit("ready");
     })
-    socket.on("incommingOffer",({offer,room})=>
-    {
-        socket.to(room).emit("incommingOffer",offer)
+    socket.on("incommingOffer", ({ offer, room }) => {
+        console.log('incomming offer');
+        socket.to(room).emit("incommingOffer", offer)
     })
-    socket.on("incommingAnswer",({answer,room})=>
-    {
+    socket.on("incommingAnswer", ({ answer, room }) => {
+        console.log('incomming answer');
         console.log(room);
-        socket.to(room).emit("incommingAnswer",answer);
+        socket.to(room).emit("incommingAnswer", answer);
     })
-    socket.on("iceCandidate",({candidate,roomId})=>
-    {
-        socket.to(roomId).emit("iceCandidate",candidate);
+    socket.on("iceCandidate", ({ candidate, roomId }) => {
+        console.log('ice comming');
+        socket.to(roomId).emit("iceCandidate", candidate);
     })
 })
